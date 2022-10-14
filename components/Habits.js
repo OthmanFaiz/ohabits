81b1aed24d520fda2317ce009eeb8db8edcadc34
Othman Faiz <info@othmanfaiz.com>
2022-10-14 23:17:14 +0300

    finshid habits css

--- components/Habits.js
+++ components/Habits.js
@@ -46,6 +46,85 @@ export default function Habits() {
                 </div>
             </div>
 
+            <div className={classes.habits_title}>
+                Todo's
+            </div>
+            <div className={classes.habits}>
+                <div className={classes.habit}>
+                    <label className={classes.habit_left}>
+                        <input type="checkbox" className={classes.checkbox} />
+                        <div className={classes.habit_name}>
+                            Call Othman
+                        </div>
+                    </label>
+                    <label className={classes.habit_right}>
+                        <input type="checkbox" className={classes.checkbox} />
+                        <div className={classes.habit_name}>
+                            Buy milk
+                        </div>
+                    </label>
+                </div>
+                <div className={classes.habit}>
+                    <label className={classes.habit_left}>
+                        <input type="checkbox" className={classes.checkbox} />
+                        <div className={classes.habit_name}>
+                            Reroll in discord
+                        </div>
+                    </label>
+                </div>
+                <input type="text" className={classes.input} placeholder="Today's tasks ..." />
+            </div>
+
+            <div className={classes.habits_title}>
+                Today's note
+            </div>
+            <textarea className={classes.textarea} placeholder="Type what's in your mind ..." />
+
+            <div className={`${classes.habits_title} ${classes.line}`}>
+                How is the day
+            </div>
+            <div className={classes.day_flex}>
+                <div className={classes.day_radio}>
+                    <input className={classes.radio} type="radio" name="day" id="1" />
+                    <label htmlFor="1">1</label>
+                </div>
+                <div className={classes.day_radio}>
+                    <input className={classes.radio} type="radio" name="day" id="2" />
+                    <label htmlFor="2">2</label>
+                </div>
+                <div className={classes.day_radio}>
+                    <input className={classes.radio} type="radio" name="day" id="3" />
+                    <label htmlFor="3">3</label>
+                </div>
+                <div className={classes.day_radio}>
+                    <input className={classes.radio} type="radio" name="day" id="4" />
+                    <label htmlFor="4">4</label>
+                </div>
+                <div className={classes.day_radio}>
+                    <input className={classes.radio} type="radio" name="day" id="5" />
+                    <label htmlFor="5">5</label>
+                </div>
+                <div className={classes.day_radio}>
+                    <input className={classes.radio} type="radio" name="day" id="6" />
+                    <label htmlFor="6">6</label>
+                </div>
+                <div className={classes.day_radio}>
+                    <input className={classes.radio} type="radio" name="day" id="7" />
+                    <label htmlFor="7">7</label>
+                </div>
+                <div className={classes.day_radio}>
+                    <input className={classes.radio} type="radio" name="day" id="8" />
+                    <label htmlFor="8">8</label>
+                </div>
+                <div className={classes.day_radio}>
+                    <input className={classes.radio} type="radio" name="day" id="9" />
+                    <label htmlFor="9">9</label>
+                </div>
+                <div className={classes.day_radio}>
+                    <input className={classes.radio} type="radio" name="day" id="10" />
+                    <label htmlFor="10">10</label>
+                </div>
+            </div>
         </div>
     )
 }
